33c45ffdc54b2bc8228618059494968d5cbb80d3
Aditi Patel <aditipn@amazon.com>
2025-09-16 13:17:12 -0500

    .docusaurus to gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/aditipn/Documents/Personas/personas-site/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/aditipn/Documents/Personas/personas-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/aditipn/Documents/Personas/personas-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/aditipn/Documents/Personas/personas-site/src/css/custom.css"),
-];
